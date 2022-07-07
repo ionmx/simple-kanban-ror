57acce67151cd8f38f9fa86e307e2b85d2d12e75
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-07 17:14:58 -0600

    Update urls

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -6,31 +6,31 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get tasks_url, as: :json
+    get board_column_tasks_url(@task.column.board_id, @task.column_id), as: :json
     assert_response :success
   end
 
   test "should create task" do
     assert_difference("Task.count") do
-      post tasks_url, params: { task: { column_id: @task.column_id, description: @task.description, position: @task.position } }, as: :json
+      post board_column_tasks_url(@task.column.board_id, @task.column_id), params: { task: { column_id: @task.column_id, description: @task.description, position: @task.position } }, as: :json
     end
 
     assert_response :created
   end
 
   test "should show task" do
-    get task_url(@task), as: :json
+    get board_column_task_url(@task.column.board_id, @task.column_id, @task), as: :json
     assert_response :success
   end
 
   test "should update task" do
-    patch task_url(@task), params: { task: { column_id: @task.column_id, description: @task.description, position: @task.position } }, as: :json
+    patch board_column_task_url(@task.column.board_id, @task.column_id, @task), params: { task: { column_id: @task.column_id, description: @task.description, position: @task.position } }, as: :json
     assert_response :success
   end
 
   test "should destroy task" do
     assert_difference("Task.count", -1) do
-      delete task_url(@task), as: :json
+      delete board_column_task_url(@task.column.board_id, @task.column_id, @task), as: :json
     end
 
     assert_response :no_content
