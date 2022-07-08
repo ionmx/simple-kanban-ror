ee331b9f9848925ba7b4db1acfe9b2dd79c2415a
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-07 21:36:57 -0600

    Test positions increments

--- test/models/column_test.rb
+++ test/models/column_test.rb
@@ -2,7 +2,19 @@ require "test_helper"
 
 class ColumnTest < ActiveSupport::TestCase
   test "Should not save Column without title" do
-    column = Board.first.columns.new
+    column = boards(:one).columns.new
     assert_not column.save, "Saved the column without title"
+  end
+
+  test "Should not save Column without board_id" do
+    column = Column.new
+    assert_not column.save, "Saved the column without board_id"
+  end
+
+  test "Should increase the position of the column" do
+    last_column = boards(:one).columns.last
+    column = boards(:one).columns.new(title: "New column")
+    assert column.save, "Could not save the column"
+    assert_equal last_column.position + 1, column.position, "The position of the column is not incremented by 1"
   end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,7 +2,19 @@ require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
   test "Should not save Task without description" do
-    task = Column.first.tasks.new
+    task = columns(:one).tasks.new
     assert_not task.save, "Saved the task without description"
+  end
+
+  test "Should not save Task without column_id" do
+    task = Task.new
+    assert_not task.save, "Saved the task without column_id"
+  end
+
+  test "Should increase the position of the task" do
+    last_task = columns(:one).tasks.last
+    task = columns(:one).tasks.new(description: "New task")
+    assert task.save, "Could not save the task"
+    assert_equal last_task.position + 1, task.position, "The position of the task is not incremented by 1"
   end
 end
