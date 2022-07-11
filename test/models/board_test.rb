75132d4a5a548e20c3e6273ea59987171428cd50
Jonathan Hernández <jonathan.hernandez@gmail.com>
2022-07-11 16:05:11 -0600

    Use double quotes

--- test/models/board_test.rb
+++ test/models/board_test.rb
@@ -8,12 +8,12 @@ class BoardTest < ActiveSupport::TestCase
 
   test "Should create default columns" do
     board = Board.new
-    board.title = 'My Board'
+    board.title = "My Board"
     assert board.save, "Saved board"
     cols = board.columns
     assert_equal 3, cols.size, "Board doesn't have 3 columns"
-    assert_equal 'To Do', cols[0].title, "First board must be called 'To Do'"
-    assert_equal 'In Proccess', cols[1].title, "Second board must be called 'In Progress'"
-    assert_equal 'Done', cols[2].title, "Third board must be called 'Done'"
+    assert_equal "To Do", cols[0].title, "First board must be called 'To Do'"
+    assert_equal "In Proccess", cols[1].title, "Second board must be called 'In Progress'"
+    assert_equal "Done", cols[2].title, "Third board must be called 'Done'"
   end
 end
